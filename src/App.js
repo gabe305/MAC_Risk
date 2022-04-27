f949e1a1db4f3839aa9b96be44cc4ca6767bc6f5
miguel787 <miguelraulcruz@gmail.com>
2022-04-26 23:45:52 -0500

    began work on service section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Nav from "./Components/Nav/nav";
 import Hero from "./Components/Hero/hero";
+import Services from "./Components/Services/services";
 
 function App() {
   return (
     <div>
       <Nav />
       <Hero />
+      <Services />
     </div>
   );
 }
