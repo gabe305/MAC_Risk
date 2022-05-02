15911fa0f653cbce94acec6574e7b80ea9d6eca7
miguel787 <miguelraulcruz@gmail.com>
2022-05-02 01:00:56 -0500

    added form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from "./Components/Hero/hero";
 import Services from "./Components/Services/services";
 import WhoWeAre from "./Components/WhoWeAre/whoWeAre";
 import Testimonials from "./Components/Testimonials/testimonials";
+import Contact from "./Components/Contact/contact";
 import "./app.scss";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Services />
       <WhoWeAre />
       <Testimonials />
+      <Contact />
     </div>
   );
 }
---
+++ src/Components/Contact/contact.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./contact.scss";
+import closeUp from "../../Assets/Images/miguel_closeup.jpg";
+
+function contact() {
+  return (
+    <div className="form">
+      <div>
+        <img src={closeUp} alt="Photo of MAC founder" className="form__photo" />
+        <form className="form__box">
+          <h2>Contact us today</h2>
+          <div className="form__label">
+            <label>Name*</label>
+            <input type="text" placeholder="Full Name" required />
+          </div>
+          <div className="form__label">
+            <label>Phone Number</label>
+            <input placeholder="123-456-7890" type="tel" />
+          </div>
+          <div className="form__label">
+            <label>Email*</label>
+            <input type="email" placeholder="email@address.com" required />
+          </div>
+          <div className="form__label">
+            <label>Description*</label>
+            <textarea placeholder="Brief Description" required></textarea>
+          </div>
+          <input type="submit" className="form__button" />
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default contact;
