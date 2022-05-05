ff95287025252873aa5bdee51e1f26f888f703da
miguel787 <miguelraulcruz@gmail.com>
2022-05-04 23:26:09 -0500

    worked on Desktop media query for nav and hero

--- src/Components/Hero/hero.jsx
+++ src/Components/Hero/hero.jsx
@@ -4,27 +4,30 @@ import "./hero.scss";
 function hero() {
   return (
     <div className="hero">
-      <h2 className="hero__title">Mac Risk Services</h2>
-      <p className="hero__desc">
-        Random information about Mac Risk Services maybe something like a motto
-      </p>
-      <a href="#contact">
-        <button className="hero__button">Contact Us</button>
-      </a>
-      <section className="hero__cardBox">
-        <div className="hero__cards">
-          <div>Texty text texters</div>
-        </div>
-        <div className="hero__cards">
-          <div>Texty text texters</div>
-        </div>
-        <div className="hero__cards">
-          <div>Texty text texters</div>
-        </div>
-        <div className="hero__cards">
-          <div>Texty text texters</div>
-        </div>
-      </section>
+      <div className="hero__box">
+        <h2 className="hero__title">First-Class Risk Services</h2>
+        <p className="hero__desc">
+          Random information about Mac Risk Services maybe something like a
+          motto
+        </p>
+        <a href="#contact">
+          <button className="hero__button">Contact Us</button>
+        </a>
+        <section className="hero__cardBox">
+          <div className="hero__cards">
+            <div>Texty text texters</div>
+          </div>
+          <div className="hero__cards">
+            <div>Texty text texters</div>
+          </div>
+          <div className="hero__cards">
+            <div>Texty text texters</div>
+          </div>
+          <div className="hero__cards">
+            <div>Texty text texters</div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
