68c6b622d672849849185f045ec1ada1f7ec7fe3
gabe305 <gabecruz718@gmail.com>
2022-07-26 12:26:29 -0400

    header component fully done for mobile and desktop

--- src/Components/Nav/nav.jsx
+++ src/Components/Nav/nav.jsx
@@ -2,25 +2,42 @@ import React, { useState } from "react";
 import hamburger from "../../Assets/Icons/hamburger.svg";
 import Hamburger from "../HamburgerModal/HamburgerModal";
 import "./nav.scss";
+import { useMediaQuery } from "react-responsive"
 
 function Nav() {
   const [showHamModal, setShowHamModal] = useState(false);
+  const isDesktop = useMediaQuery({ query: '(min-width: 1049px)'});
 
   const handleShowModal = () => {
     setShowHamModal(!showHamModal)
   }
 
-  return (
-    <div className="nav">
-      <section className="nav__bigBox">
-        <div className="nav__top-container">
-          <h3 className="nav__title">MAC Risk Services</h3>
-          <img src={hamburger} alt="hamburger menu icon" onClick={handleShowModal} className="nav__hamburgerButton" />
-        </div>
-        {showHamModal ? <Hamburger showHamModal={showHamModal} setShowHamModal={setShowHamModal}/> : null}
-      </section>
-    </div>
-  );
+  if(isDesktop) {
+    return (
+      <div className="nav">
+        <section className="nav__bigBox">
+          <div className="nav__top-container">
+            <h3 className="nav__title">MAC Risk Services</h3>
+            {showHamModal ? <Hamburger showHamModal={showHamModal} setShowHamModal={setShowHamModal}/> : null}
+            <img src={hamburger} alt="hamburger menu icon" onClick={handleShowModal} className="nav__hamburgerButton" />
+          </div>
+        </section>
+      </div>
+    )
+  } else {
+    return(
+      <div className="nav">
+        <section className="nav__bigBox">
+          <div className="nav__top-container">
+            <h3 className="nav__title">MAC Risk Services</h3>
+            <img src={hamburger} alt="hamburger menu icon" onClick={handleShowModal} className="nav__hamburgerButton" />
+          </div>
+          {showHamModal ? <Hamburger showHamModal={showHamModal} setShowHamModal={setShowHamModal}/> : null}
+        </section>
+      </div>
+    )
+  }  
+
 }
 
 export default Nav;
