4522444002bf430007e87caf8d435c186037eac8
gabe305 <gabecruz718@gmail.com>
2022-07-26 02:47:59 -0400

    got hero page for desktop

--- src/Components/Nav/nav.jsx
+++ src/Components/Nav/nav.jsx
@@ -15,7 +15,7 @@ function Nav() {
       <section className="nav__bigBox">
         <div className="nav__top-container">
           <h3 className="nav__title">MAC Risk Services</h3>
-          <img src={hamburger} alt="hamburger menu icon" onClick={handleShowModal} className="nav_hamburgerButton" />
+          <img src={hamburger} alt="hamburger menu icon" onClick={handleShowModal} className="nav__hamburgerButton" />
         </div>
         {showHamModal ? <Hamburger showHamModal={showHamModal} setShowHamModal={setShowHamModal}/> : null}
       </section>
